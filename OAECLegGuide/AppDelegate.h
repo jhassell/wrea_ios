8cb80d5dd2331e0f83565ab9a25416cc30698be4
Matt Galloway <matt@architactile.com>
2012-07-21 17:52:22 -0500

    Initial Commit

---
+++ OAECLegGuide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OAECLegGuide
+//
+//  Created by Matt Galloway on 7/21/12.
+//  Copyright (c) 2012 Architactile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OAECLegGuide/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  OAECLegGuide
+//
+//  Created by Matt Galloway on 7/21/12.
+//  Copyright (c) 2012 Architactile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
